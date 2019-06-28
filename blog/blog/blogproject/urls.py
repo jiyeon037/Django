3265639e26f3a1a3e956ab0851acec89b67794ef
jiyeon037 <jiyeon7269@naver.com>
2019-06-28 20:23:29 +0900

    템플릿 상속, url 관리

--- blog/blog/blogproject/urls.py
+++ blog/blog/blogproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blogapp.views
 from django.conf import settings # media
 from django.conf.urls.static import static # media
@@ -7,9 +7,7 @@ from django.conf.urls.static import static # media
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name = "home"),
-    path('blog/<int:blog_id>', blogapp.views.detail, name="detail"),
-    path('blog/new/', blogapp.views.new, name='new'),
-    path('blog/create', blogapp.views.create, name='create'),
+    path('blog/', include('blogapp.urls')),
     path('portfolio', blogapp.views.portfolio, name='portfolio'),
     # url을 만들어준다고 무조건 html에 연결되는게 아님!
     # path('어떤 url이 들어오면', (어디에 있는)어떤 함수 실행시켜라)
