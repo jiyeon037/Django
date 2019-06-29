89bc82d26dcf268a086c26cc8ccfd2bd419fa0dd
jiyeon037 <jiyeon7269@naver.com>
2019-06-30 01:53:02 +0900

    회원가입, 로그인

--- blog/blog/accounts/views.py
+++ blog/blog/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -8,24 +8,24 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
             auth.login(request, user) # 회원가입 후 자동 로그인
-            return redirect('blog')
+            return redirect('home')
     return render(request, 'signup.html')
 
 def login(request):
     if request.method == 'POST':
             username = request.POST['username']
             password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None: # 유저가 존재하는 회원이라면
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None: # 유저가 존재하는 회원이라면
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'error': 'username or password is incorrect.'})
     else:
         return render(request, 'login.html')
 
 def logout(request):
-    if request.method == 'POST'
+    if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
     return render(request, 'login.html')
